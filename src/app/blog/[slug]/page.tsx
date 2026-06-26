2a8530f282390d04aefb8fa2f362fe12722e4b2f
pdombe <pradnyafarkande65@gmail.com>
2026-06-26 18:20:01 +0530

    og tag changes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -164,7 +164,7 @@ export default async function BlogDetailsPage({ params }: Props) {
     </a>
 
     {/* Twitter */}
-    <a
+    {/* <a
       href={`https://twitter.com/intent/tweet?url=https://nextgenlg.com.au/blog/${post.slug}&text=${encodeURIComponent(post.title)}`}
       target="_blank"
       rel="noopener noreferrer"
@@ -172,7 +172,7 @@ export default async function BlogDetailsPage({ params }: Props) {
       className="w-9 h-9 rounded-full bg-primary flex items-center justify-center hover:bg-[#9C9C9C] transition-colors"
     >
       <Twitter fill="currentColor" strokeWidth={0} className="w-4 h-4 text-[#FFFFFF]" />
-    </a>
+    </a> */}
 
     {/* LinkedIn */}
     
@@ -187,7 +187,7 @@ export default async function BlogDetailsPage({ params }: Props) {
 
     {/* WhatsApp */}
     
-    <a  href={`https://wa.me/?text=${encodeURIComponent(post.title + " - https://nextgenlg.com.au/blog/" + post.slug)}`}
+    {/* <a  href={`https://wa.me/?text=${encodeURIComponent(post.title + " - https://nextgenlg.com.au/blog/" + post.slug)}`}
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Share on WhatsApp"
@@ -197,7 +197,7 @@ export default async function BlogDetailsPage({ params }: Props) {
         <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347z"/>
         <path d="M12 0C5.373 0 0 5.373 0 12c0 2.123.554 4.135 1.535 5.879L.057 23.882a.5.5 0 0 0 .61.641l6.188-1.453A11.94 11.94 0 0 0 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0zm0 21.894a9.878 9.878 0 0 1-5.031-1.378l-.361-.214-3.733.877.947-3.645-.235-.374A9.867 9.867 0 0 1 2.106 12C2.106 6.533 6.533 2.106 12 2.106c5.467 0 9.894 4.427 9.894 9.894 0 5.467-4.427 9.894-9.894 9.894z"/>
       </svg>
-    </a>
+    </a> */}
 
   </div>
 </div>
