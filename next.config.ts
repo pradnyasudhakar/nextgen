ce5933c94a49a346776d85ab6ff9f338f61820ef
pdombe <pradnyafarkande65@gmail.com>
2026-08-10 16:21:18 +0530

    new changes

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: "/webp-img/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
     ];
   },
 };
